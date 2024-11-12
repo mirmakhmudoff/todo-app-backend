eaef9456eabfa0e54001e90e94a0f1623c3d47c5
Javokhir Mirmakhmudov <138425315+mirmakhmudoff@users.noreply.github.com>
2024-11-12 19:56:16 +0500

    Finished

--- apps/todos/urls.py
+++ apps/todos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TodoListCreateView, TodoStatusUpdateView
+from .views import TodoListCreateView, TodoStatusUpdateView, TodoDeleteView
 
 urlpatterns = [
     path('', TodoListCreateView.as_view(), name='todo-list-create'),
     path('<int:pk>/status/', TodoStatusUpdateView.as_view(), name='todo-status-update'),
+    path('<int:pk>/delete/', TodoDeleteView.as_view(), name='todo-delete'),
 ]
